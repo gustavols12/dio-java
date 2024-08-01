39ec7a1d6db9278dc9ee6aa084d5fa8af31675a7
gustavo <gustavols2100@gmail.com>
2024-07-31 21:37:57 -0300

    Adicionando novas váriaveis

--- projeto-java/src/App.java
+++ projeto-java/src/App.java
@@ -1,5 +1,15 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int nota = 5;
+        int media = 7;
+        if(nota >= media){
+            System.out.println("Aprovado");
+        }
+        else if(nota < media && nota > 6){
+            System.out.println("Aluno em recuperação");
+        }
+        else{
+            System.out.println("Reprovado");
+        }
     }
 }
