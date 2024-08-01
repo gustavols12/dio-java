8f6480054b7d9aec1c7148b6ac37a7807c25ebe7
gustavo <gustavols2100@gmail.com>
2024-07-31 21:02:24 -0300

    Adicionando novas váriaveis

--- projeto-java/src/MinhaClasse.java
+++ projeto-java/src/MinhaClasse.java
@@ -1,5 +1,8 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("teste");
+        String nome = "Gustavo";
+        System.out.println(String.format("meu nome é %s",nome));
+
     }
 }
