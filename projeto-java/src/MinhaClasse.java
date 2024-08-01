477381ebeeb1a34200c93a1c5976978104ef56ba
gustavo <gustavols2100@gmail.com>
2024-07-31 21:27:01 -0300

    Adicionando novas váriaveis

--- projeto-java/src/MinhaClasse.java
+++ projeto-java/src/MinhaClasse.java
@@ -2,7 +2,15 @@ public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("teste");
         String nome = "Gustavo";
-        System.out.println(String.format("meu nome é %s",nome));
+        String sobrenome = "Luiz";
+        int idade = 23;
+        System.out.println(String.format("meu nome é %s e minha idade é %d",nome, idade));
+        System.out.println((nome_completo(nome, sobrenome)));
+
+
+    }
+    public static String nome_completo(String nome, String sobrenome){
+        return String.format("Mue nome completo é %s %s", nome,sobrenome);
 
     }
 }
