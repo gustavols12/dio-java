a26b5600555c1e2f18dff78ad053dd228c99b96f
gustavo <gustavols2100@gmail.com>
2024-08-05 12:30:05 -0300

    alterando a estrutura

--- projeto-java/src/JavaBasico/comandosBasicos.java
+++ projeto-java/src/JavaBasico/comandosBasicos.java
@@ -1,3 +1,5 @@
+package JavaBasico;
+
 public class comandosBasicos {
     public static void main(String[] args) {
         
--- projeto-java/src/JavaBasico/tiposVariaveis.java
+++ projeto-java/src/JavaBasico/tiposVariaveis.java
@@ -1,3 +1,5 @@
+package JavaBasico;
+
 public class tiposVariaveis {
     public static void main(String[] args) {
         System.out.println("teste");
